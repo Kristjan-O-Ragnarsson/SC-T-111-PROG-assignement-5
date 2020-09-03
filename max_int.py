92054899fe9f4bacfed7a82676c75e778de52dcb
Kristjan O. Ragnarsson <kristjan@ragnarsson.im>
2020-09-03 09:53:20 +0000

    Implemented the algorithum

--- max_int.py
+++ max_int.py
@@ -3,3 +3,15 @@ while input not negativer
     if input > higest in 
         higest = input
 """
+
+max_int = 0
+kill = False
+
+while not kill:
+    num_int = int(input("Input a number: "))
+    if num_int < 0:
+        kill = True
+    elif num_int > max_int:
+        max_int = num_int
+else:
+    print("The maximum is", max_int)
