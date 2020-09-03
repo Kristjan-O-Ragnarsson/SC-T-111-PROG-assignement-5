8355d7bffc49ed512d9c12f65001d62738314af4
Kristjan O. Ragnarsson <kristjan@ragnarsson.im>
2020-09-03 10:13:18 +0000

    Global vars and basic logic implemented

--- sequence.py
+++ sequence.py
@@ -12,4 +12,18 @@ n = 2
 n1 = 1
 n2 = 0
 
+counter = 2
+
+limit = int(input())
+
+print(n1)
+print(n)
+
+while counter < limit:
+    tmp = n
+    n = n + n1 + n2
+    n2 = n1
+    n1 = tmp
+    print(n)
+    counter += 1
 
